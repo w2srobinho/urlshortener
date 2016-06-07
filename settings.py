4cc02647c2b7f75ae7212dc09402c669a7f9d4ad
Willian de Souza <willianstosouza@gmail.com>
2016-06-07 15:45:50 -0300

    update postgresql to get environment variable

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 HOSTNAME = ''
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost/url_shortener"
+SQLALCHEMY_DATABASE_URI = os.environ['POSTGRESQL_DATABASE']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
